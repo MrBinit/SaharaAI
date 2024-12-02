ddec74668ebdedc9ab29c0f0688ed4f09f650e88
MrBinit <sapkotabinit2002@gmail.com>
2024-12-02 23:26:23 +0545

    Done with agent memory now need to fix internet access but api is not working

--- custom_agent.py
+++ custom_agent.py
@@ -4,55 +4,81 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.agents.format_scratchpad.openai_tools import (
     format_to_openai_tool_messages,
 )
+from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain.agents import AgentExecutor
 from langchain.agents.output_parsers.openai_tools import OpenAIToolsAgentOutputParser
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_google_community import GoogleSearchAPIWrapper
 
-llm = ChatOllama(
-    model = "llama3.1",
-    temperature = 0, 
-    verbose= True
-)
+from dotenv import load_dotenv
+import os
 
-@tool
-def get_word_length(word: str) -> int:
-    """
-    This tool returns the length of a given word.
-    """
-    return len(word)
+google_api_key = os.getenv("Google_API_key")
 
-tools = [get_word_length]
+# google wrapper
+search = GoogleSearchAPIWrapper(google_api_key= google_api_key)
 
-prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            "You are an Nepalese Historian who known detail history of Nepal.",
-        ),
-        ("user", "{input}"),
-        MessagesPlaceholder(variable_name = "agent_scratchpad"),
 
-    ]
-)
 
-llm_with_tools = llm.bind_tools(tools)
+# llm = ChatOllama(
+#     model = "llama3.1",
+#     temperature = 0, 
+#     verbose= True
+# )
+# memory = ChatMessageHistory(session_id = "test-session")
 
-agent = (
-    {
-        "input" : lambda x: x["input"],
-        "agent_scratchpad": lambda x: format_to_openai_tool_messages(
-            x["intermediate_steps"]
-       ),
-    }
-    | prompt
-    | llm_with_tools
-    | OpenAIToolsAgentOutputParser()
-)
+# @tool
+# def get_word_length(word: str) -> int:
+#     """
+#     This tool returns the length of a given word.
+#     """
+#     return len(word)
 
-#Create an agent  executor by passing in the agent and tools
-agent_executor = AgentExecutor(agent = agent, tools = tools, verbose = True)
-result = agent_executor.invoke({"input" :"Who is Prithvi Narayan Shah? "})
+# tools = [get_word_length]
 
-if result:
-    print(f"[Output] --> {result['output']}")
-else:
-    print('There is no result.')
+# prompt = ChatPromptTemplate.from_messages(
+#     [
+#         (
+#             "system",
+#             "You are an Nepalese Historian who known detail history of Nepal.",
+#         ),
+#         ("user", "{input}"),
+#         MessagesPlaceholder(variable_name = "agent_scratchpad"),
+
+#     ]
+# )
+
+# llm_with_tools = llm.bind_tools(tools)
+
+# agent = (
+#     {
+#         "input" : lambda x: x["input"],
+#         "agent_scratchpad": lambda x: format_to_openai_tool_messages(
+#             x["intermediate_steps"]
+#        ),
+#     }
+#     | prompt
+#     | llm_with_tools
+#     | OpenAIToolsAgentOutputParser()
+# )
+
+# #Create an agent  executor by passing in the agent and tools
+# agent_executor = AgentExecutor(agent = agent, tools = tools, verbose = True)
+
+# # memory in agent
+# agent_with_chat_history = RunnableWithMessageHistory(
+#     agent_executor,
+#     lambda session_id: memory,
+#     input_message_key = "input",
+#     history_messages_key = "chat_history"
+
+# )
+# result = agent_with_chat_history.invoke({"input" :"Who is Prithvi Narayan Shah? "},
+#                                         config={"configurable": {"session_id": "test-session"}},
+
+#                                         )
+
+# if result:
+#     print(f"[Output] --> {result['output']}")
+# else:
+#     print('There is no result.')
