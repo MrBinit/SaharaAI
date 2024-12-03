b1aeb55be6a544beaeb9fc1a8401416c43ae1aca
MrBinit <sapkotabinit2002@gmail.com>
2024-12-03 08:38:21 +0545

    done with google search wrapper

--- custom_agent.py
+++ custom_agent.py
@@ -4,6 +4,7 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.agents.format_scratchpad.openai_tools import (
     format_to_openai_tool_messages,
 )
+from langchain_core.tools import Tool
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain.agents import AgentExecutor
 from langchain.agents.output_parsers.openai_tools import OpenAIToolsAgentOutputParser
@@ -13,19 +14,27 @@ from langchain_google_community import GoogleSearchAPIWrapper
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
 google_api_key = os.getenv("Google_API_key")
-
+google_cse_id = os.getenv("GOOGLE_CSE_ID")
 # google wrapper
-search = GoogleSearchAPIWrapper(google_api_key= google_api_key)
 
+search = GoogleSearchAPIWrapper(google_api_key=google_api_key, google_cse_id = google_cse_id, k = 10)
+tools = [
+    Tool(
+    name = "google_search",
+    description= "search about Nepal's History",
+    func = search.run,
+    )
+]
 
-
-# llm = ChatOllama(
-#     model = "llama3.1",
-#     temperature = 0, 
-#     verbose= True
-# )
-# memory = ChatMessageHistory(session_id = "test-session")
+llm = ChatOllama(
+    model = "llama3.1",
+    temperature = 0, 
+    verbose= True
+)
+memory = ChatMessageHistory(session_id = "test-session")
 
 # @tool
 # def get_word_length(word: str) -> int:
@@ -36,49 +45,49 @@ search = GoogleSearchAPIWrapper(google_api_key= google_api_key)
 
 # tools = [get_word_length]
 
-# prompt = ChatPromptTemplate.from_messages(
-#     [
-#         (
-#             "system",
-#             "You are an Nepalese Historian who known detail history of Nepal.",
-#         ),
-#         ("user", "{input}"),
-#         MessagesPlaceholder(variable_name = "agent_scratchpad"),
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are an Nepalese Historian who known detail history of Nepal.",
+        ),
+        ("user", "{input}"),
+        MessagesPlaceholder(variable_name = "agent_scratchpad"),
 
-#     ]
-# )
+    ]
+)
 
-# llm_with_tools = llm.bind_tools(tools)
+llm_with_tools = llm.bind_tools(tools)
 
-# agent = (
-#     {
-#         "input" : lambda x: x["input"],
-#         "agent_scratchpad": lambda x: format_to_openai_tool_messages(
-#             x["intermediate_steps"]
-#        ),
-#     }
-#     | prompt
-#     | llm_with_tools
-#     | OpenAIToolsAgentOutputParser()
-# )
+agent = (
+    {
+        "input" : lambda x: x["input"],
+        "agent_scratchpad": lambda x: format_to_openai_tool_messages(
+            x["intermediate_steps"]
+       ),
+    }
+    | prompt
+    | llm_with_tools
+    | OpenAIToolsAgentOutputParser()
+)
 
-# #Create an agent  executor by passing in the agent and tools
-# agent_executor = AgentExecutor(agent = agent, tools = tools, verbose = True)
+#Create an agent  executor by passing in the agent and tools
+agent_executor = AgentExecutor(agent = agent, tools = tools, verbose = True)
 
-# # memory in agent
-# agent_with_chat_history = RunnableWithMessageHistory(
-#     agent_executor,
-#     lambda session_id: memory,
-#     input_message_key = "input",
-#     history_messages_key = "chat_history"
+# memory in agent
+agent_with_chat_history = RunnableWithMessageHistory(
+    agent_executor,
+    lambda session_id: memory,
+    input_message_key = "input",
+    history_messages_key = "chat_history"
 
-# )
-# result = agent_with_chat_history.invoke({"input" :"Who is Prithvi Narayan Shah? "},
-#                                         config={"configurable": {"session_id": "test-session"}},
+)
+result = agent_with_chat_history.invoke({"input" :"Who is Prithvi Narayan Shah? "},
+                                        config={"configurable": {"session_id": "test-session"}},
 
-#                                         )
+                                        )
 
-# if result:
-#     print(f"[Output] --> {result['output']}")
-# else:
-#     print('There is no result.')
+if result:
+    print(f"[Output] --> {result['output']}")
+else:
+    print('There is no result.')
